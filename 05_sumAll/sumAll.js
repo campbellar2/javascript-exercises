7c20e94a6d9a47e212795706112b69a655a05200
Alex Campbell <campbellar2@vcu.edu>
2022-03-29 23:05:46 -0400

    Completed Exercise 5, matched solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != "number" || typeof(num2) != "number") { return "ERROR";} 
+    if (num1 < 0 || num2 < 0) { return "ERROR";} 
+    let sum = 0;
+    let i = Math.min(...arguments);
+    do {
+        sum += i;
+        i += 1;
+    } while (i <= Math.max(...arguments));
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;

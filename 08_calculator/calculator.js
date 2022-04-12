2e9378fe4353bea974c596e4577a18cb18c88ee5
Alex Campbell <campbellar2@vcu.edu>
2022-04-12 11:35:14 -0400

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let sum = 0
+	for (i = 0; i < array.length; i++) {
+        sum += array[i]
+    };
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    let sum = 1;
+    for (i = 0; i < array.length; i++) {
+        sum *= array[i];
+    }
+    return sum;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+    let sum = 1
+    for (i = 1; i <= exp; i++) {
+        sum *= base
+    };
+    return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+        return -1;
+    } else if (num === 0) {
+        return 1;
+    } else {
+        return (num * factorial(num -1))
+    }
 };
 
 // Do not edit below this line

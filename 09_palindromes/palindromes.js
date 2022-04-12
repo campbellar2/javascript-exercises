65ebae331ee0f3df91f5340cd3560c302003adf8
Alex Campbell <campbellar2@vcu.edu>
2022-04-12 16:49:55 -0400

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string.toLowerCase().replace(/[^a-z]/g, "");
+    let reversed = cleanString.split("").
+            reverse().
+            join("");
+    return (cleanString == reversed)
 };
 
 // Do not edit below this line
